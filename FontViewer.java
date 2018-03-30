5f5562476be72c7c5e904f77f53c6b1aca03bdf7
Jonathan Hector <jonathan@hectorgraphics.ch>
2018-03-30 02:15:55 +0200

    MVP(Minimum Valuable Product Done.)

--- FontViewer.java
+++ FontViewer.java
@@ -1,20 +1,18 @@
-package ex1v4;
+package ex1;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
-import javafx.beans.binding.ObjectBinding;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
@@ -23,81 +21,73 @@ import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-
 public class FontViewer extends Application {
-    private String[] typeFace = {"Serif", "Sans-Serif", "Monospaced"};
+
+    private Label label;
+    private Slider slider;
+    private String[] typeFace = {"Serif", "SansSerif", "Monospaced"};
     private String[] typeStyle = {"Regular", "Italic", "Bold"};
-    private Label label = new Label("Big Java");
-    //    private ListView<String> fontsInstalled = new ListView<>(FXCollections.observableList(Font.getFamilies()));
-    private ListView<String> fontsInstalled = new ListView<>(FXCollections.observableArrayList(typeFace[0], typeFace[1], typeFace[2]));
-    private ListView<String> fontsStyle = new ListView<>(FXCollections.observableArrayList("regular", "italic", "bold"));
+    private Font font;
+    private ObservableList<String> fonts = FXCollections.observableArrayList(typeFace[0], typeFace[1], typeFace[2]);
+    private ComboBox<String> familyChoice;
 
-    private Font fontBoldItalic = Font.font(label.getFont().getFamily(),
-            FontWeight.BOLD, FontPosture.ITALIC, 35);
-    private Font fontBold = Font.font(label.getFont().getFamily(),
-            FontWeight.BOLD, FontPosture.REGULAR, 35);
-    private Font fontItalic = Font.font(label.getFont().getFamily(),
-            FontWeight.NORMAL, FontPosture.ITALIC, 35);
-    private Font fontNormal = Font.font(label.getFont().getFamily(),
-            FontWeight.NORMAL, FontPosture.REGULAR, 35);
+    private Insets insets = new Insets(10);
 
-    private Slider fontSizeSlider = new Slider(10, 70, 35);
-    private Spinner<Integer> spinner = new Spinner<>(10, 70, 35);
-
-    private int fontSize;
-
-
-    public static void main(String[] args) {
-        launch(args);
+    {
+        slider = new Slider(10, 70, 35);
+        label = new Label("Big Java");
+        label.setFont(Font.font(slider.getValue()));
+        label.setPadding(new Insets(10));
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        label.setStyle(getFontSize());
-        label.setPadding(new Insets(15));
-        VBox titleLabel = new VBox(label);
-        titleLabel.setAlignment(Pos.CENTER_LEFT);
+    public void start(Stage primaryStage) throws Exception {
 
-        VBox cover = new VBox(new ImageView(getClass().getResource("../BigJava.jpg").toExternalForm()));
-        cover.setAlignment(Pos.CENTER_RIGHT);
+        AnchorPane anchorPane = new AnchorPane();
+        ImageView image = new ImageView(getClass().getResource("../BigJava.jpg").toExternalForm());
+        AnchorPane.setTopAnchor(image, 10.0);
+        AnchorPane.setRightAnchor(image, 10.0);
+        anchorPane.getChildren().add(image);
 
-        HBox box = new HBox(label, cover);
-        box.setPadding(new Insets(10));
-        box.setAlignment(Pos.CENTER);
+        HBox cover = new HBox(label, anchorPane);
+        cover.setAlignment(Pos.CENTER);
+        cover.setPadding(insets);
 
-        box.setAlignment(Pos.CENTER);
-        VBox styleBox = new VBox(fontSizeViewer(), comboBox(), checkBox());
-        styleBox.setAlignment(Pos.CENTER);
-        // TODO: The menu doesn't work if this bit of code is activated!!!
+        VBox centerBottomPane = new VBox(fontSizeViewer(), comboBox(), checkBox());
+        centerBottomPane.setAlignment(Pos.CENTER);
+        centerBottomPane.setPadding(insets);
 
-        BorderPane bottomHandler = new BorderPane();
-        bottomHandler.setCenter(styleBox);
-        bottomHandler.setBottom(radioButtonSizeSetter());
-        bottomHandler.setLeft(sliderBox());
+        BorderPane bottomPane = new BorderPane();
+        bottomPane.setLeft(sliderBox());
+        bottomPane.setCenter(centerBottomPane);
+        bottomPane.setBottom(radioButtonSizeSetter());
 
-        VBox root = new VBox(menu(), box, bottomHandler);
+        BorderPane mainPane = new BorderPane();
+        mainPane.setTop(menu());
+        mainPane.setCenter(cover);
+        mainPane.setBottom(bottomPane);
 
+        Scene scene = new Scene(mainPane);
 
-        Scene scene = new Scene(root);
+        // Updates the ComoBox but does lazy Binding
+        // Click in the Scene the update in the ComboBox after clicking in the Menu
+        scene.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> {
+            if (!label.getFont().getFamily().equalsIgnoreCase(familyChoice.getSelectionModel().getSelectedItem())) {
+                familyChoice.getSelectionModel().select(label.getFont().getFamily());
+            }
 
-        scene.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
-            System.out.println("i'M IN THE SCENE");
-            if (menu().isPressed())
-                comboBox().isDisable();
+            if (label.fontProperty().equals("italic")) System.out.println(label.fontProperty().toString());
+            System.out.println(label.getFont());
         });
 
-        scene.addEventHandler(MouseEvent.MOUSE_CLICKED, e ->
-                System.out.println("I'm back")
+        scene.addEventFilter(Event.ANY, event ->
+                primaryStage.sizeToScene()
         );
 
         primaryStage.setTitle("Font Viewer");
         primaryStage.setScene(scene);
-        primaryStage.sizeToScene();
+        primaryStage.hide();
         primaryStage.show();
-    }
-
-    private MouseEvent handler(MouseEvent e) {
-        return e;
     }
 
     /*  ================
@@ -105,19 +95,140 @@ public class FontViewer extends Application {
      *  ================
      */
     private VBox sliderBox() {
-        fontSizeSlider.setOrientation(Orientation.VERTICAL);
-        fontSizeSlider.setShowTickLabels(true);
-        fontSizeSlider.setShowTickMarks(true);
-        fontSizeSlider.setMajorTickUnit(5);
-        fontSizeSlider.setMinorTickCount(3);
-        fontSizeSlider.setSnapToTicks(true);
-        fontSizeSlider.setPadding(new Insets(10));
-        VBox sliderBox = new VBox(fontSizeSlider);
+        slider.setOrientation(Orientation.VERTICAL);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+        slider.setMajorTickUnit(5);
+        slider.setMinorTickCount(3);
+        slider.setSnapToTicks(true);
+        slider.setPadding(new Insets(10));
+        VBox sliderBox = new VBox(slider);
         sliderBox.setPadding(new Insets(0));
         sliderBox.setAlignment(Pos.CENTER_LEFT);
         sliderBox.setMaxWidth(Double.MAX_VALUE);
 
         return sliderBox;
+    }
+
+    /*  ================
+     *      Menu
+     *  ================
+     */
+    private MenuBar menu() {
+        MenuBar menuBar = new MenuBar();
+        final Menu FILEMENU = new Menu("_File");
+        final Menu FACEMENU = new Menu("Face");
+        MenuItem serifMenu = new MenuItem(typeFace[0]);
+        MenuItem sansSerifMenu = new MenuItem(typeFace[1]);
+        MenuItem monospacedMenu = new MenuItem(typeFace[2]);
+        FACEMENU.getItems().addAll(serifMenu, sansSerifMenu, monospacedMenu);
+
+        FACEMENU.showingProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                label.fontProperty().unbind(); // By unbinding it, I'm able to change the whole font
+                serifMenu.setOnAction(e -> label.setFont(Font.font(typeFace[0], getFontSize())));
+                sansSerifMenu.setOnAction(e -> label.setFont(Font.font(typeFace[1], getFontSize())));
+                monospacedMenu.setOnAction(e -> label.setFont(Font.font(typeFace[2], getFontSize())));
+            }
+        });
+
+
+        final Menu STYLEMENU = new Menu("_Style");
+        CheckMenuItem regularMenu = new CheckMenuItem(typeStyle[0]);
+        regularMenu.setAccelerator(new KeyCodeCombination(KeyCode.I, KeyCombination.CONTROL_DOWN));
+        CheckMenuItem italicMenu = new CheckMenuItem(typeStyle[1]);
+        italicMenu.setAccelerator(new KeyCodeCombination(KeyCode.I, KeyCombination.CONTROL_DOWN));
+        CheckMenuItem boldMenu = new CheckMenuItem(typeStyle[2]);
+        boldMenu.setAccelerator(new KeyCodeCombination(KeyCode.B, KeyCombination.CONTROL_DOWN));
+        STYLEMENU.getItems().addAll(regularMenu, italicMenu, boldMenu);
+
+        STYLEMENU.showingProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                label.fontProperty().unbind();
+
+                regularMenu.setOnAction(e -> label.setFont(styleSetter("regular")));
+                italicMenu.setOnAction(e -> label.setFont(styleSetter("italic")));
+                boldMenu.setOnAction(e -> label.setFont(styleSetter("bold")));
+            }
+        });
+
+        FILEMENU.getItems().addAll(FACEMENU, STYLEMENU);
+        menuBar.getMenus().addAll(FILEMENU);
+
+        return menuBar;
+    }
+
+    private Font styleSetter(String fontStyle) {
+
+        switch (fontStyle.toLowerCase()) {
+            case "regular":
+                return Font.font(label.getFont().getFamily(), FontPosture.REGULAR, getFontSize());
+
+            case "italic":
+                return Font.font(label.getFont().getFamily(), FontPosture.ITALIC, getFontSize());
+
+            case "bold":
+                return Font.font(label.getFont().getFamily(), FontWeight.BOLD, getFontSize());
+
+            case "italicBold":
+                return Font.font(label.getFont().getFamily(), FontWeight.BOLD, FontPosture.ITALIC, getFontSize());
+        }
+        return null;
+    }
+
+
+    /*  ===========================================
+     *      It displays the size of the font used
+     *  ===========================================
+     */
+    private HBox fontSizeViewer() {
+        Label fontSizeLabel = new Label();
+        fontSizeLabel.textProperty().bind(Bindings.concat("Font size: ",
+                slider.valueProperty().asString("%3.0f")));
+
+        HBox sizeBox = new HBox(fontSizeLabel);
+        sizeBox.setAlignment(Pos.CENTER);
+        sizeBox.setPadding(new Insets(10, 0, 10, 0));
+        return sizeBox;
+    }
+
+    /*  ================
+     *     Combo Box
+     *  ================
+     */
+    private ComboBox comboBox() {
+
+// FIXME: Menu doesn't apply the functions if this code is running but the Slider works.
+        familyChoice = new ComboBox<>(fonts);
+        familyChoice.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+                label.setFont(Font.font(newValue, getFontSize()))
+        );
+
+        familyChoice.setPromptText("Please choose a font");
+        return familyChoice;
+    }
+
+    /*  ================
+     *     CheckBox
+     *  ================
+     */
+    private HBox checkBox() {
+
+        CheckBox italic = new CheckBox("italic");
+        italic.setPadding(new Insets(20, 10, 0, 10));
+        CheckBox bold = new CheckBox("bold");
+        bold.setPadding(new Insets(20, 10, 0, 10));
+
+
+        italic.selectedProperty().addListener((observable, oldValue, newValue) ->
+                italic.setOnAction(e -> label.setFont(styleSetter("italic"))));
+
+        bold.selectedProperty().addListener((observable, oldValue, newValue) ->
+                bold.setOnAction(e -> label.setFont(styleSetter("bold"))));
+
+        HBox checkBox = new HBox(italic, bold);
+        checkBox.setAlignment(Pos.CENTER);
+        return checkBox;
     }
 
     /*  =====================================
@@ -138,10 +249,9 @@ public class FontViewer extends Application {
 
         type.selectedToggleProperty().addListener((ov, old_toggle, new_toggle) -> {
             if (type.getSelectedToggle() != null) {
-                if (small.isArmed()) setFontSize(20);
-                if (medium.isArmed()) setFontSize(40);
-                if (large.isArmed()) setFontSize(60);
-
+                if (small.isArmed()) setFontSize(24);
+                if (medium.isArmed()) setFontSize(36);
+                if (large.isArmed()) setFontSize(48);
             }
         });
 
@@ -150,139 +260,15 @@ public class FontViewer extends Application {
         return radioBtnBox;
     }
 
-    /*  ================
-     *     Combo Box
-     *  ================
-     */
-    private ComboBox comboBox() {
-
-// FIXME: Menu doesn't apply the functions if this code is running but the Slider works.
-        ComboBox<String> familyChoice = new ComboBox<>(FXCollections.observableArrayList(typeFace[0], typeFace[1], typeFace[2]));
-
-        // FIXME: Works only but the Menu doesn't
-        if (familyChoice.isVisible()) {
-            label.fontProperty().bind(Bindings.createObjectBinding(() ->
-                            Font.font(familyChoice.getValue(),
-                                    fontSizeSlider.getValue()),
-                    fontSizeSlider.valueProperty(),
-                    familyChoice.valueProperty()));
-        } else {
-            label.textProperty().unbindBidirectional(familyChoice);
-        }
-
-        return familyChoice;
+    private int getFontSize() {
+        return (int) slider.getValue();
     }
 
-
-
-    /*  ================
-     *     CheckBox
-     *  ================
-     */
-    private HBox checkBox() {
-
-        CheckBox italic = new CheckBox("italic");
-        italic.setPadding(new Insets(20, 10, 0, 10));
-        CheckBox bold = new CheckBox("bold");
-        bold.setPadding(new Insets(20, 10, 0, 10));
-
-        EventHandler<ActionEvent> handler = e -> {
-            if (bold.isSelected() && italic.isSelected()) {
-                label.setFont(fontBoldItalic); // Both check boxes checked
-            } else if (bold.isSelected()) {
-                label.setFont(fontBold); // The Bold check box checked
-            } else if (italic.isSelected()) {
-                label.setFont(fontItalic); // The Italic check box checked
-            } else {
-                label.setFont(fontNormal); // Both check boxes unchecked
-            }
-        };
-
-
-        italic.setOnAction(handler);
-        bold.setOnAction(handler);
-
-        HBox checkBox = new HBox(italic, bold);
-        checkBox.setAlignment(Pos.CENTER);
-        return checkBox;
+    private void setFontSize(double fontSize) {
+        slider.setValue(fontSize);
     }
 
-
-    /*  ==========
-     *     MENU
-     *  ==========
-     */
-    private HBox menu() {
-
-        MenuBar menuBar = new MenuBar();
-        Menu file = new Menu("_File");
-        Menu face = new Menu("Face");
-        Menu style = new Menu("Style");
-        Menu exitOption = new Menu("_Quit");
-        exitOption.setOnAction(e -> Platform.exit());
-
-        MenuItem faceType1 = new MenuItem(typeFace[0]);
-        faceType1.setOnAction(e -> label.setFont(Font.font(faceType1.getText())));
-
-        MenuItem faceType2 = new MenuItem(typeFace[1]);
-
-        MenuItem faceType3 = new MenuItem(typeFace[2]);
-
-        face.getItems().addAll(faceType1, faceType2, faceType3);
-
-        MenuItem regularStyle = new MenuItem(typeStyle[0]);
-        MenuItem italicStyle = new MenuItem(typeStyle[1]);
-        MenuItem boldStyle = new MenuItem(typeStyle[2]);
-//        regularStyle.setOnAction(e -> label.setStyle("-fx-font-style: " + regularStyle.getText().toLowerCase()));
-//        italicStyle.setOnAction(e -> label.setStyle("-fx-font-style: " + italicStyle.getText().toLowerCase()));
-//        boldStyle.setOnAction(e -> label.setStyle("-fx-font-weight: " + boldStyle.getText().toLowerCase()));
-
-
-        style.getItems().addAll(regularStyle, italicStyle, boldStyle);
-
-        file.getItems().addAll(face, style, new SeparatorMenuItem(), exitOption);
-        file.setAccelerator(KeyCombination.keyCombination("Shortcut+f"));
-
-
-        menuBar.getMenus().add(file);
-        HBox menuBox = new HBox(menuBar);
-        menuBox.setMaxWidth(Double.MAX_VALUE);
-        return menuBox;
-    }
-
-
-    /*  ===========================================
-     *      It displays the size of the font used
-     *  ===========================================
-     */
-    private HBox fontSizeViewer() {
-        Label fontSizeLabel = new Label();
-        fontSizeLabel.textProperty().bind(Bindings.concat("Font value: ",
-                fontSizeSlider.valueProperty().asString("%3.0f")));
-
-        HBox sizeBox = new HBox(fontSizeLabel);
-        sizeBox.setAlignment(Pos.CENTER);
-        sizeBox.setPadding(new Insets(10, 0, 10, 0));
-        return sizeBox;
-    }
-
-    private Label createLabel(String family) {
-        label = new Label("Boo");
-        label.setFont(Font.font(family));
-        label.setStyle(getFontSize());
-        label.setPadding(new Insets(10));
-        System.out.println(family);
-        return label;
-    }
-
-    // TODO: The spinner doesn't return the value
-    private String getFontSize() {
-        if (!fontSizeSlider.isValueChanging()) fontSize = (int) fontSizeSlider.getValue();
-        label.setFont(Font.font(fontSize));
-        return "-fx-font-size: " + fontSize + ";";
-    }
-
-    private void setFontSize(int n) {
-        fontSizeSlider.setValue(n);
+    public static void main(String[] args) {
+        launch(args);
     }
 }
